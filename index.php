510c2df8a61c824340ec15b01285513526776f31
khaiquang-27 <quangkhai271125@gmail.com>
2025-04-19 21:22:21 +0700

    update index.php

--- index.php
+++ index.php
@@ -1,14 +1,11 @@
 <?php
-
 if (!isset($_SESSION)) {
     session_start();
 }
-ob_start(); // Bắt đầu bộ đệm đầu ra
-
+ob_start();
 
 include './models/pdo.php';
 include './views/header.php';
-
 include './models/nguoidung.php';
 include './models/binhluan.php';
 include './models/sanpham.php';
@@ -22,162 +19,159 @@ if (isset($_SESSION['thongbao'])) {
     unset($_SESSION['thongbao']);
 }
 
-
 $product_new = loadall_product_home();
 $product_iphone = loadall_product_iphone();
 $product_samsung = loadall_product_samsung();
 $product_top8_sale = loadall_top8_product();
-$product_iphone_top8 =loadall_top8_iphone();
-// $product_new2 = loadall_product_home2();
+$product_iphone_top8 = loadall_top8_iphone();
+
 if (isset($_GET['act']) && ($_GET['act'] != "")) {
     $act = $_GET['act'];
     switch ($act) {
         case 'dangky':
-
             if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $ten = $_POST['ten'];
                 $email = $_POST['email'];
                 $so_dien_thoai = $_POST['so_dien_thoai'];
                 $dia_chi = $_POST['dia_chi'];
-                $mat_khau = password_hash($_POST['mat_khau'], PASSWORD_BCRYPT);
-                $hinh = isset($_FILES['anh_dai_dien']['name']) ? $_FILES['anh_dai_dien']['name'] : '';
-                $target_dir = "./uploads/";
-                $target_file = $target_dir . basename($hinh);
-                if (!empty($hinh) && move_uploaded_file($_FILES["anh_dai_dien"]["tmp_name"], $target_file)) {
-                } else {
-                    $hinh = '';
-                }
-                if (emailExists($email)) {
-                    // var_dump(emailExists($email));
-                    // die();
-                    $thongbao = "Email này đã được sử dụng!";
+                $mat_khau = $_POST['mat_khau'];
+                $re_mat_khau = $_POST['re_mat_khau'];
+
+                if ($mat_khau !== $re_mat_khau) {
+                    $thongbao = "⚠️ Mật khẩu nhập lại không khớp!";
                     require './views/account/dangky.php';
                     return;
                 }
-                insert_user($ten, $email, $mat_khau, $hinh, $so_dien_thoai, $dia_chi);
-                header('Location:index.php?act=dangnhap');
+
+                $hashed_password = password_hash($mat_khau, PASSWORD_BCRYPT);
+                $hinh = '';
+                if (isset($_FILES['anh_dai_dien']) && $_FILES['anh_dai_dien']['error'] === 0) {
+                    $hinh = $_FILES['anh_dai_dien']['name'];
+                    move_uploaded_file($_FILES["anh_dai_dien"]["tmp_name"], "./uploads/" . $hinh);
+                }
+
+                if (emailExists($email)) {
+                    $thongbao = "⚠️ Email này đã được sử dụng!";
+                    require './views/account/dangky.php';
+                    return;
+                }
+
+                insert_user($ten, $email, $hashed_password, $hinh, $so_dien_thoai, $dia_chi);
+                header('Location: index.php?act=dangnhap');
                 exit();
             }
-
             include './views/account/dangky.php';
             break;
 
         case 'dangnhap':
-
             if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $email = $_POST['email'];
                 $password = $_POST['password'];
-
                 $user = findByEmail($email);
-                if ($user && password_verify($password, $user['mat_khau'])) {
-                    $_SESSION['user'] = $user;
-                    //  var_dump($_SESSION['user']['loai_nguoi_dung']);
-                    //  die();
 
-                    if ($_SESSION['user']['trang_thai'] == '0') {
+                if ($user && password_verify($password, $user['mat_khau'])) {
+                    if ($user['trang_thai'] == '0') {
                         $thongbao = "Tài khoản đã bị khóa.";
-                        unset($_SESSION['user']);
                     } else {
-                        if ($_SESSION['user']['loai_nguoi_dung'] == 'KhachHang') {
+                        $_SESSION['user'] = $user;
+                        if ($user['loai_nguoi_dung'] == 'KhachHang') {
                             header('Location:index.php');
                         } else {
                             header('Location:./admin/index.php');
                         }
+                        exit();
                     }
                 } else {
                     $thongbao = "Email hoặc mật khẩu không đúng!";
-                    require './views/account/dangnhap.php';
                 }
             }
-
             include './views/account/dangnhap.php';
             break;
 
-        case 'dangxuat';
-            session_start();
+        case 'dangxuat':
             session_destroy();
             header('Location:index.php');
             break;
 
-        case 'shopiphone':
-            if (isset($_POST['kyw']) && ($_POST['kyw'] != "")) {
-                $kyw = $_POST['kyw'];
-            } else {
-                $kyw = "";
-            }
-            $product_shop_iphone = loadall_shopiphone($kyw);
-            include './views/shop/shop-iphone.php';
-            break;
-
-        case 'shopsamsung':
-            if (isset($_POST['kyw']) && ($_POST['kyw'] != "")) {
-                $kyw = $_POST['kyw'];
-            } else {
-                $kyw = "";
-            }
-            $product_shop_samsung = loadall_shopsamsung($kyw);
-            include './views/shop/shop-samsung.php';
-            break;
-
-        case 'shopxiaomi':
-            if (isset($_POST['kyw']) && ($_POST['kyw'] != "")) {
-                $kyw = $_POST['kyw'];
-            } else {
-                $kyw = "";
-            }
-            $product_shop_xiaomi = loadall_shopxiaomi($kyw);
-            include './views/shop/shop-xiaomi.php';
-            break;
-
-        case 'chitietsanpham':
-            if (isset($_GET['ma_san_pham']) && ($_GET['ma_san_pham'] > 0)) {
-                $ma_san_pham = $_GET['ma_san_pham'];
-                $oneproduct = loadone_sanpham($ma_san_pham);
-                extract($oneproduct);
-                $product_cung_loai = load_product_cungloai($ma_danh_muc, $ma_san_pham);
-                $load_all_binhluan = load_all_binhluan($ma_san_pham);
-                // var_dump($product_cung_loai);
-
-                // $list_variant = load_product_variant($product_id);
-
-                // if (isset($_SESSION['username'])) {
-                //     $list_img_cart = list_img_cart($_SESSION['username']['user_id']);
-                // }
-                include './views/chitietsanpham.php';
-            } else {
-                include './views/home.php';
-            }
-            break;
-        case 'chinhsach':
-
-            include './views/chinhsach.php';
-            break;
-        case 'vechungtoi':
-
-            include './views/vechungtoi.php';
-            break;
-
-        case 'addtocart':
-            if (!isset($_SESSION['cart'])) {
-                $_SESSION['cart'] = [];
-            }
-
-            if (isset($_GET['act']) && $_GET['act'] === 'addtocart' && $_SERVER['REQUEST_METHOD'] === 'POST') {
-                $id_san_pham = $_POST['ma_san_pham'];
-                $ten_san_pham = $_POST['ten_san_pham'];
-                $gia = $_POST['gia'];
-                $so_luong = (int)$_POST['so_luong'];
-                $mau_sac = $_POST['mau_sac'];
-                $anh_san_pham = $_POST['anh_san_pham'];
-
-
-                // Kiểm tra sản phẩm đã tồn tại trong giỏ hàng chưa
-                $found = false;
-                foreach ($_SESSION['cart'] as &$item) {
-                    if ($item['id'] === $id_san_pham && $item['mau_sac'] === $mau_sac) {
-                        $item['so_luong'] += $so_luong; // Cộng dồn số lượng
-                        $found = true;
-                        break;
+            case 'shopiphone':
+                if (isset($_POST['kyw']) && ($_POST['kyw'] != "")) {
+                    $kyw = $_POST['kyw'];
+                } else {
+                    $kyw = "";
+                }
+                $product_shop_iphone = loadall_shopiphone($kyw);
+                include './views/shop/shop-iphone.php';
+                break;
+    
+            case 'shopsamsung':
+                if (isset($_POST['kyw']) && ($_POST['kyw'] != "")) {
+                    $kyw = $_POST['kyw'];
+                } else {
+                    $kyw = "";
+                }
+                $product_shop_samsung = loadall_shopsamsung($kyw);
+                include './views/shop/shop-samsung.php';
+                break;
+    
+            case 'shopxiaomi':
+                if (isset($_POST['kyw']) && ($_POST['kyw'] != "")) {
+                    $kyw = $_POST['kyw'];
+                } else {
+                    $kyw = "";
+                }
+                $product_shop_xiaomi = loadall_shopxiaomi($kyw);
+                include './views/shop/shop-xiaomi.php';
+                break;
+    
+            case 'chitietsanpham':
+                if (isset($_GET['ma_san_pham']) && ($_GET['ma_san_pham'] > 0)) {
+                    $ma_san_pham = $_GET['ma_san_pham'];
+                    $oneproduct = loadone_sanpham($ma_san_pham);
+                    extract($oneproduct);
+                    $product_cung_loai = load_product_cungloai($ma_danh_muc, $ma_san_pham);
+                    $load_all_binhluan = load_all_binhluan($ma_san_pham);
+                    // var_dump($product_cung_loai);
+    
+                    // $list_variant = load_product_variant($product_id);
+    
+                    // if (isset($_SESSION['username'])) {
+                    //     $list_img_cart = list_img_cart($_SESSION['username']['user_id']);
+                    // }
+                    include './views/chitietsanpham.php';
+                } else {
+                    include './views/home.php';
+                }
+                break;
+            case 'chinhsach':
+    
+                include './views/chinhsach.php';
+                break;
+            case 'vechungtoi':
+    
+                include './views/vechungtoi.php';
+                break;
+    
+            case 'addtocart':
+                if (!isset($_SESSION['cart'])) {
+                    $_SESSION['cart'] = [];
+                }
+    
+                if (isset($_GET['act']) && $_GET['act'] === 'addtocart' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+                    $id_san_pham = $_POST['ma_san_pham'];
+                    $ten_san_pham = $_POST['ten_san_pham'];
+                    $gia = $_POST['gia'];
+                    $so_luong = (int)$_POST['so_luong'];
+                    $mau_sac = $_POST['mau_sac'];
+                    $anh_san_pham = $_POST['anh_san_pham'];
+    
+    
+                    // Kiểm tra sản phẩm đã tồn tại trong giỏ hàng chưa
+                    $found = false;
+                    foreach ($_SESSION['cart'] as &$item) {
+                        if ($item['id'] === $id_san_pham && $item['mau_sac'] === $mau_sac) {
+                            $item['so_luong'] += $so_luong; // Cộng dồn số lượng
+                            $found = true;
+                            break;
                     }
                 }
 
@@ -325,7 +319,7 @@ if (isset($_GET['act']) && ($_GET['act'] != "")) {
                             }
                             update_user($ma_nguoi_dung, $ho_ten, $email, $so_dien_thoai, $dia_chi,$hinh);
                     
-                            // Cập nhật thông tin sessio
+                            // Cập nhật thông tin session
                             $_SESSION['user']['ten'] = $ho_ten;
                             $_SESSION['user']['email'] = $email;
                             $_SESSION['user']['so_dien_thoai'] = $so_dien_thoai;
@@ -354,6 +348,14 @@ if (isset($_GET['act']) && ($_GET['act'] != "")) {
                         }
                         
                         break;
+                        case 'huy_donhang':
+                            if (isset($_POST['ma_don_hang'])) {
+                                $ma_don_hang = $_POST['ma_don_hang'];
+                                huy_donhang($ma_don_hang);
+                            }
+                            header("Location: index.php?act=donhangcuatoi");
+                            exit();
+                        
 
 
 
